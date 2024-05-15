aff6f3d41167379874023eeb45833198fd509e06
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-15 21:14:17 +0700

    v2

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,26 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-
-interface Division {
-  [x: string]: any;
-  division: string;
-  viewDivision: string;
-}
-interface Fac {
-  [x: string]: any;
-  Fac: string;
-  viewValue: string;
-}
 
 
-interface usefor {
-  [x: string]: any;
-  usefor: string;
-  viewusefor: string;
-}
 
 @Component({
   selector: 'app-request',
@@ -30,39 +10,44 @@ interface usefor {
 
 
 export class RequestComponent {
-  divisions: Division[] = [
-    { division: '---', viewDivision: '---' },
-    { division: 'GM', viewDivision: 'GM' },
-    { division: 'M/A', viewDivision: 'M/A' },
-    { division: 'tPMA', viewDivision: 'PMA' },
-    { division: 'Medical', viewDivision: 'Medical' },
+  selectedDivision: string;
+  selectedFac: string;
+  selectedProcess: string;
+  selectedUseFor: string;
+
+  divisions = [
+    { division: 'div1', viewDivision: 'Division 1' },
+    { division: 'div2', viewDivision: 'Division 2' },
+    { division: 'div3', viewDivision: 'Division 3' },
+    // Add more divisions as needed
   ];
 
-  selectedDivision: string = '---';
-
-  Facs: Fac[] = [
-    { Fac: '---', viewValue: '---' },
-    { Fac: '1', viewValue: '1' },
-    { Fac: '2', viewValue: '2' },
-    { Fac: '3', viewValue: '3' },
-    { Fac: '4', viewValue: '4' },
-    { Fac: '5', viewValue: '5' },
-    { Fac: 'Other', viewValue: 'Other' },
+  facs = [
+    { fac: 'fac1', viewValue: 'Facility 1' },
+    { fac: 'fac2', viewValue: 'Facility 2' },
+    { fac: 'fac3', viewValue: 'Facility 3' },
+    // Add more facilities as needed
   ];
 
-  selectedFac: string = '---';
-
-
-
-  usefors: usefor[] = [
-    { usefor: '---', viewusefor: '---' },
-    { usefor: 'Sorting', viewusefor: 'Sorting' },
-    { usefor: 'F/A', viewusefor: 'F/A' },
-    { usefor: 'Rework', viewusefor: 'Rework' },
-    { usefor: 'Mass Fac', viewusefor: 'Mass Fac' },
-    { usefor: 'Pre Fac', viewusefor: 'Pre Fac' },
+  processes = [
+    { process: 'proc1', viewProcess: 'Process 1' },
+    { process: 'proc2', viewProcess: 'Process 2' },
+    { process: 'proc3', viewProcess: 'Process 3' },
+    // Add more processes as needed
   ];
 
-  selectedusefor: string = '---';
+  useFors = [
+    { useFor: 'use1', viewUseFor: 'Use for 1' },
+    { useFor: 'use2', viewUseFor: 'Use for 2' },
+    { useFor: 'use3', viewUseFor: 'Use for 3' },
+    // Add more use-for options as needed
+  ];
 
+  constructor() {
+    // Initialize selected values if needed
+    this.selectedDivision = this.divisions[0].division;
+    this.selectedFac = this.facs[0].fac;
+    this.selectedProcess = this.processes[0].process;
+    this.selectedUseFor = this.useFors[0].useFor;
+  }
 }
