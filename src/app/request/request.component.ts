168c684dc1ceb10cebf7b3b69d8e60b5d8c9cc2a
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-24 17:12:38 +0700

    v16

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
 import { switchMap, of, catchError } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ToolDetail } from './tool-detail.model';
+import { response } from 'express';
 
 @Component({
   selector: 'app-request',
@@ -9,14 +13,23 @@ import { switchMap, of, catchError } from 'rxjs';
   styleUrls: ['./request.component.scss'],
 })
 export class RequestComponent implements OnInit {
-  onPartNoSelected(_t31: string) {
-    throw new Error('Method not implemented.');
-  }
-  requestForm!: FormGroup;
-
+  requestForm: FormGroup;
   partNoSuggestions: string[] = [];
   processOptions: string[] = [];
   MCOptions: string[] = [];
+  dataSource = new MatTableDataSource<ToolDetail[]>();
+  // displayedColumns: string[] = ['CT_sec', 'Division',  'DwgNo', 'DwgRev', 'DwgUpdate', 'ItemNo','MC', 'Master_Tooling_ID','PartNo','Process', 'Spec', 'Usage_pcs'];
+  public displayedColumns = [
+    'PartNo',
+    'Process',
+    'MC',
+    'ItemNo',
+    'Spec',
+    'Usage_pcs',
+  ];
+
+  @ViewChild(MatSort) sort!: MatSort;
+
   selectedDivision: string;
   divisions = [
     { division: '---', viewDivision: '---' },
@@ -25,9 +38,15 @@ export class RequestComponent implements OnInit {
   ];
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder
   ) {
     this.selectedDivision = this.divisions[0].division;
+    this.requestForm = this.fb.group({
+      PartNo: [''],
+      Process: [''],
+      MC: [''],
+    });
   }
 
   ngOnInit(): void {
@@ -36,55 +55,59 @@ export class RequestComponent implements OnInit {
       Process: [''],
       MC: [''],
     });
+    this.dataSource.sort = this.sort;
   }
 
+  getProcess() {
+    const PartNo = this.requestForm.get('PartNo')?.value;
+    this.authService.Post_Process({ PartNo }).subscribe((processResponse) => {
+      console.log(processResponse);
+      if (processResponse.length > 0 && processResponse[0].length > 0) {
+        this.processOptions = processResponse[0].map(
+          (item: any) => item.Process
+        );
+      }
+    });
+  }
+
+  getMC() {
+    const PartNo = this.requestForm.get('PartNo')?.value;
+    const Process = this.requestForm.get('Process')?.value;
+
+    if (PartNo && Process) {
+      this.authService.GetMC({ PartNo, Process }).subscribe(
+        (MCResponse) => {
+          console.log(MCResponse);
+          if (MCResponse.length > 0 && MCResponse[0].length > 0) {
+            this.MCOptions = MCResponse[0].map((item: any) => item.MC);
+          } else {
+            console.error('MC options not found.');
+          }
+        },
+        (MCError) => {
+          console.error('Error fetching MC options:', MCError);
+        }
+      );
+    }
+  }
   onSubmit() {
-    if (this.requestForm.valid) {
-      const PartNo = this.requestForm.get('PartNo')?.value;
-      const Process = this.requestForm.get('Process')?.value;
-      const MC = this.requestForm.get('MC')?.value;
-
-      this.authService.Post_Process({ PartNo }).subscribe((processResponse) => {
-        console.log(processResponse);
-        if (processResponse.length > 0 && processResponse[0].length > 0) {
-          this.processOptions = processResponse[0].map(
-            (item: any) => item.Process
-          );
-        }
-
-        // Check if both PartNo and Process have values
-        if (PartNo && Process) {
-          this.authService.GetMC({ PartNo, Process }).subscribe(
-            (MCResponse) => {
-              console.log(MCResponse);
-              if (MCResponse.length > 0 && MCResponse[0].length > 0) {
-                this.MCOptions = MCResponse[0].map((item: any) => item.MC);
-              } else {
-                console.error('MC options not found.');
-              }
-            },
-            (MCError) => {
-              console.error('Error fetching MC options:', MCError);
-            }
-          );
-        }
-        // Check if PartNo, Process, and MC are available to show Tool details
-        if (PartNo && Process && MC) {
-          this.authService
-            .Post_ToolDetial({ PartNo, Process, MC })
-            .subscribe((ToolResponse) => {
-              console.log(ToolResponse);
-            });
-        }
+    const PartNo = this.requestForm.get('PartNo')?.value;
+    const Process = this.requestForm.get('Process')?.value;
+    const MC = this.requestForm.get('MC')?.value;
+    if (PartNo && Process && MC) {
+      const data = {
+        PartNo: PartNo,
+        Process: Process,
+        MC: MC,
+      };
+      console.log(PartNo, Process, MC);
+      this.authService.Post_ToolDetial(data).subscribe({
+        next: (response) => {
+          this.dataSource = new MatTableDataSource(response);
+          // this.dataSource.sort = this.sort;mater
+          console.log(this.dataSource);
+        },
       });
     }
   }
 }
-
-// onSubmit() {
-//   if (this.requestForm.valid) {
-//     const PartNo = this.requestForm.get('PartNo')?.value;
-//     const Process = this.requestForm.get('Process')?.value;
-//     const MC = this.requestForm.get('MC')?.value;
-
-// }
---
+++ src/app/request/tool-detail.model.ts
@@ -0,0 +1,10 @@
+// tool-detail.model.ts
+export interface ToolDetail {
+  PartNo: string;
+  Process: string;
+  MC: string;
+  ItemNo: string;
+  Spec: string;
+  Usage_pcs: number;
+
+}
