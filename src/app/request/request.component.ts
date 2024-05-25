6e71baa294c98faea379e87321cd6f417d246120
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-25 11:29:15 +0700

    v18

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -8,12 +8,20 @@ import { MatPaginator } from '@angular/material/paginator';
 
 
 interface ToolDetail {
+  No: number;
   PartNo: string;
   ItemNo: string;
   MC: string;
   Process: string;
   Spec: string;
   Usage_pcs: number;
+  Qty: number;
+  Result1: string;
+  Result2: string;
+  Result3: string
+  Result4: string;
+  Result5: string;
+  Result6: string;
 }
 
 @Component({
@@ -28,35 +36,69 @@ export class RequestComponent implements OnInit {
   MCOptions: string[] = [];
   dataSource = new MatTableDataSource<ToolDetail>();
   displayedColumns: string[] = [
+    'select',
+    'No',
     'PartNo',
     'ItemNo',
     'MC',
     'Process',
     'Spec',
     'Usage_pcs',
+    'Qty',
+    'Result1',
+    'Result2',
+    'Result3',
+    'Result4',
+    'Result5',
+    'Result6',
   ];
+  selection = new SelectionModel<ToolDetail>(true, []);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   selectedDivision: string;
+  selectedCase: string;
+
   divisions = [
     { division: '---', viewDivision: '---' },
     { division: 'GM', viewDivision: 'GM' },
     { division: 'PMA', viewDivision: 'PMA' },
   ];
+  Cases = [
+    { Case: '---', viewCase: '---' },
+    { Case: 'BRO', viewCase: 'BRO' },
+    { Case: 'BUR', viewCase: 'BUR' },
+    { Case: 'USA', viewCase: 'USA' },
+    { Case: 'HOL', viewCase: 'HOL' },
+    { Case: 'INV', viewCase: 'INV' },
+    { Case: 'MOD', viewCase: 'MOD' },
+    { Case: 'NON', viewCase: 'NON' },
+    { Case: 'RET', viewCase: 'RET' },
+    { Case: 'SET', viewCase: 'SET' },
+    { Case: 'SPA', viewCase: 'SPA' },
+    { Case: 'STO', viewCase: 'STO' },
+    { Case: 'CHA', viewCase: 'CHA' },
+  ];
+
 
   constructor(
     private formBuilder: FormBuilder,
     private authService: AuthService
   ) {
     this.selectedDivision = this.divisions[0].division;
+    this.selectedCase = this.Cases[0].Case;
+
     this.requestForm = this.formBuilder.group({
       PartNo: [''],
       Process: [''],
       MC: [''],
     });
   }
-
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
   ngOnInit(): void {
     this.dataSource.sort = this.sort;
   }
@@ -107,5 +149,22 @@ export class RequestComponent implements OnInit {
         },
       });
     }
+  }
+
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  masterToggle() {
+    this.isAllSelected() ? this.selection.clear() : this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  checkboxLabel(row?: ToolDetail): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.No + 1}`;
   }
 }
