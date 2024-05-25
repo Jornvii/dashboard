da27087becffccb65202998b02f647ce4d72121e
gvorn <157733779+gvorn7@users.noreply.github.com>
2024-05-25 10:56:54 +0700

    v17

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../service/auth.service';
-import { switchMap, of, catchError } from 'rxjs';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import { ToolDetail } from './tool-detail.model';
-import { response } from 'express';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+
+
+interface ToolDetail {
+  PartNo: string;
+  ItemNo: string;
+  MC: string;
+  Process: string;
+  Spec: string;
+  Usage_pcs: number;
+}
 
 @Component({
   selector: 'app-request',
@@ -17,13 +26,12 @@ export class RequestComponent implements OnInit {
   partNoSuggestions: string[] = [];
   processOptions: string[] = [];
   MCOptions: string[] = [];
-  dataSource = new MatTableDataSource<ToolDetail[]>();
-  // displayedColumns: string[] = ['CT_sec', 'Division',  'DwgNo', 'DwgRev', 'DwgUpdate', 'ItemNo','MC', 'Master_Tooling_ID','PartNo','Process', 'Spec', 'Usage_pcs'];
-  public displayedColumns = [
+  dataSource = new MatTableDataSource<ToolDetail>();
+  displayedColumns: string[] = [
     'PartNo',
-    'Process',
-    'MC',
     'ItemNo',
+    'MC',
+    'Process',
     'Spec',
     'Usage_pcs',
   ];
@@ -36,13 +44,13 @@ export class RequestComponent implements OnInit {
     { division: 'GM', viewDivision: 'GM' },
     { division: 'PMA', viewDivision: 'PMA' },
   ];
+
   constructor(
     private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private fb: FormBuilder
+    private authService: AuthService
   ) {
     this.selectedDivision = this.divisions[0].division;
-    this.requestForm = this.fb.group({
+    this.requestForm = this.formBuilder.group({
       PartNo: [''],
       Process: [''],
       MC: [''],
@@ -50,18 +58,12 @@ export class RequestComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.requestForm = this.formBuilder.group({
-      PartNo: [''],
-      Process: [''],
-      MC: [''],
-    });
     this.dataSource.sort = this.sort;
   }
 
   getProcess() {
     const PartNo = this.requestForm.get('PartNo')?.value;
     this.authService.Post_Process({ PartNo }).subscribe((processResponse) => {
-      console.log(processResponse);
       if (processResponse.length > 0 && processResponse[0].length > 0) {
         this.processOptions = processResponse[0].map(
           (item: any) => item.Process
@@ -77,7 +79,6 @@ export class RequestComponent implements OnInit {
     if (PartNo && Process) {
       this.authService.GetMC({ PartNo, Process }).subscribe(
         (MCResponse) => {
-          console.log(MCResponse);
           if (MCResponse.length > 0 && MCResponse[0].length > 0) {
             this.MCOptions = MCResponse[0].map((item: any) => item.MC);
           } else {
@@ -90,22 +91,19 @@ export class RequestComponent implements OnInit {
       );
     }
   }
+
   onSubmit() {
     const PartNo = this.requestForm.get('PartNo')?.value;
     const Process = this.requestForm.get('Process')?.value;
     const MC = this.requestForm.get('MC')?.value;
+
     if (PartNo && Process && MC) {
-      const data = {
-        PartNo: PartNo,
-        Process: Process,
-        MC: MC,
-      };
-      console.log(PartNo, Process, MC);
+      const data = { PartNo, Process, MC };
       this.authService.Post_ToolDetial(data).subscribe({
         next: (response) => {
-          this.dataSource = new MatTableDataSource(response);
-          // this.dataSource.sort = this.sort;mater
-          console.log(this.dataSource);
+          if (response.length > 0 && response[0].length > 0) {
+            this.dataSource.data = response[0] as ToolDetail[];
+          }
         },
       });
     }
